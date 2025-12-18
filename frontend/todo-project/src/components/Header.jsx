e7ba3e6b10fc8b6d30c7c4d82409f9154ff44d8d
yushinKim <dbtls0218@naver.com>
2025-12-19 08:18:21 +0900

    test github actions

--- frontend/todo-project/src/components/Header.jsx
+++ frontend/todo-project/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header({ onRefresh, onDeleteDone, loading }) {
   return (
     <div className="header">
-      <h1 style={{ margin: 0 }}>Todo</h1>
+      <h1 style={{ margin: 0 }}>Todo 리스트</h1>
 
       <div className="header-actions">
         <button className="btn-ghost" onClick={onRefresh} disabled={loading}>
@@ -18,4 +18,3 @@ export default function Header({ onRefresh, onDeleteDone, loading }) {
     </div>
   );
 }
-
